6f5141c51f82355cfa2629cfc116c449db2e5e43
manuel15939 <manuel15939@gmail.com>
2023-06-08 20:19:55 -0500

    reasignado y redeclarando el tipo de variables

---
+++ src/scope/re.js
@@ -0,0 +1,31 @@
+var firstName; // undefined / declarando
+firstName = 'Manuel'; // asignando
+console.log(firstName);
+
+var lastName = 'Andres'; // declarando / asignando 
+lastName = 'Mariana'; // reasignando
+console.log(lastName);
+
+var secondName = 'Andres'; // declarando / asignando
+var secondName = 'Mariana'; // redeclarando / reasignando
+console.log(secondName);
+
+// let 
+let fruit = 'Apple'; // declarando y asignando
+fruit = 'Kiwi'; // reasignando
+console.log(fruit);
+
+let fruit = 'Banana'; // redeclarando y reasignando
+
+// const
+const animal = 'dog'; // declarando y asignando
+animal = 'cat'; // reasignando
+const animal = 'snake' // redeclarando y reasignando
+console.log(animal);
+
+const vehicules = [];
+vehicules.push('🚗')
+console.log(vehicules);
+
+vehicules.pop();
+console.log(vehicules);
